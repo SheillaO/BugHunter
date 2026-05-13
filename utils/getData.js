86d223d0e4a0cc49618edd667221649c9e912009
SheillaO <sheilla.alusa@strathmore.edu>
2026-05-13 18:22:49 +0300

    Update getData.js

--- utils/getData.js
+++ utils/getData.js
@@ -1,16 +1,17 @@
-import fs from 'node:fs/promises'
-import path from 'node:path'
+import fs from "node:fs/promises";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export async function getData() {
-
-  try { 
-    const pathJSON = path.join('data', 'data.json')
-    const data = await fs.readFile(pathJSON, 'utf8')
-    const parsedData = JSON.parse(data)
-    return parsedData
-
-  } catch(err) {
-    console.log(err)
-    return []
+  try {
+    // Change 'data.json' to 'bugs.json'
+    const pathJSON = path.join(__dirname, "..", "data", "bugs.json");
+    const data = await fs.readFile(pathJSON, "utf8");
+    return JSON.parse(data);
+  } catch (err) {
+    console.error("Warning: bugs.json could not be read:", err.message);
+    return [];
   }
 }
