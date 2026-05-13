74ff5dd729ad4f14a44f2ef489891aff96a88711
SheillaO <sheilla.alusa@strathmore.edu>
2026-05-13 20:28:47 +0300

    Update bugEvents.js

--- events/bugEvents.js
+++ events/bugEvents.js
@@ -1,7 +1,9 @@
-import { EventEmitter } from 'node:events'
-import { createAlert } from '../utils/createAlert.js'
+import { EventEmitter } from "node:events";
+// 1. Point to the correct file name that exists in your utils folder
+import { createSlackAlert } from "../utils/createSlackAlert.js";
 
-export const sightingEvents = new EventEmitter()
+// 2. Rename variable to match your updated routing logic
+export const bugEvents = new EventEmitter();
 
-sightingEvents.on('sighting-added', createAlert)
- 
+// 3. Listen for the correct platform hook name
+bugEvents.on("bug-added", createSlackAlert);
